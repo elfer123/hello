1564529c13b50cec08c91b1637212f4b6d0f10dc
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:07:59 -0500

    editado

--- hello/main.cpp
+++ hello/main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "joaquin" << endl;
+    int x=45;
+    int y=4;
+    int z=x+y;
+    int a=x-y;
+    int b=x*y;
+    double c=x/y;
+    cout<<"el resultado de la suma es :"<<z<<endl;
+    cout<<"el resultado de la resta es :"<<a<<endl;
+    cout<<"el resultado de la multiplicacion es :"<<b<<endl;
+    cout<<"el resultado de la division es :"<<c<<endl;
     return 0;
+
+
 }
